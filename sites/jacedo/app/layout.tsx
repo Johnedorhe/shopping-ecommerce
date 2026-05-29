52339f6b4fa8cf94c43ae106ddb6bca3eff2674a
John <cooljohne@gmail.com>
2026-05-30 00:42:42 +0100

    header and footer component added to layout

--- sites/jacedo/app/layout.tsx
+++ sites/jacedo/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/ui/header";
+import Footer from "@/components/ui/footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,7 +24,17 @@ export default function RootLayout({
       lang="en"
       className={`${inter.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <head>
+          <Header />
+        </head>
+
+        <main>
+        {children}
+        </main>
+
+        <Footer />
+        </body>
     </html>
   );
 }
